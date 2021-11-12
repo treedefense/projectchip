322cc8fb8a60fe8fe4f45bba569b2c7c13bb07be
Ronnie Smith <halfpint1170@gmail.com>
2021-11-11 21:01:39 -0800

    starting playing a round of golf

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import './App.css';
 
+interface Hole {
+  id: number;
+  par: number;
+}
+
+interface Course {
+  name: string;
+  holes: Hole[];
+}
+
+function HoleView(hole: Hole) {
+  return <p>Hole {hole.id} is a par {hole.par}</p>
+}
+
+function CourseView(course: Course) {
+  return (
+    <div className="course">
+      <p>Course name is {course.name}</p>
+      {course.holes.map(hole => {
+        return <HoleView {...hole} />
+      })}
+    </div>
+  )
+}
+
 function App() {
+  const course: Course = {
+    name: "Chipville",
+    holes: [
+      {
+        id: 1,
+        par: 3,
+      },
+      {
+        id: 2,
+        par: 4,
+      },
+      {
+        id: 3,
+        par: 5,
+      }
+    ]
+  };
+
   return (
     <div className="App">
+      <CourseView {...course} />
     </div>
   );
 }
