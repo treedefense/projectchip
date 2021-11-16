05ceb004d033f06d264b1e24254e09e160cbf3cf
Phil <pahartman1@crimson.ua.edu>
2021-11-15 21:41:32 -0800

    added buttons and score tracking

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 interface Hole {
   id: number;
   par: number;
+  strokes: number;
+  setStrokes(strokes: number): void;
 }
 
-interface Course {
+interface Round {
   name: string;
   holes: Hole[];
 }
 
 function HoleView(hole: Hole) {
-  return <p>Hole {hole.id} is a par {hole.par}</p>
+  if(hole.strokes > 0) {
+    return <p>Shot {hole.strokes} on hole {hole.id}</p>
+  }
+
+  return (
+    <>
+      <p>Enter Strokes for hole {hole.id}</p>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((strokes) => {
+        return (
+          <button
+            key={strokes}
+            onClick={() => hole.setStrokes(strokes)}
+          >
+            {strokes}
+          </button>
+        )
+      })}
+    </>
+  )
 }
 
-function CourseView(course: Course) {
+function RoundView(round: Round) {
+  let score = 0;
+  for (let i = 0; i < round.holes.length; i++) {
+    const hole = round.holes[i];
+    if (hole.strokes === 0) {
+      break;
+    }
+    score += hole.strokes - hole.par;
+  }
+
   return (
-    <div className="course">
-      <p>Course name is {course.name}</p>
-      {course.holes.map(hole => {
-        return <HoleView {...hole} />
+    <div className="round">
+      <p>Course name is {round.name}</p>
+      {round.holes.map(hole => {
+        return <HoleView {...hole} key={hole.id} />
       })}
+      Score: {score}
     </div>
   )
 }
 
+function useNewHole(id: number, par: number): Hole {
+    const [strokes, setStrokes] = useState(0)
+    return {
+      id,
+      par,
+      strokes,
+      setStrokes,
+    };
+}
+
 function App() {
-  const course: Course = {
+  const round: Round = {
     name: "Chipville",
     holes: [
-      {
-        id: 1,
-        par: 3,
-      },
-      {
-        id: 2,
-        par: 4,
-      },
-      {
-        id: 3,
-        par: 5,
-      }
+      useNewHole(1, 3),
+      useNewHole(2, 5),
+      useNewHole(3, 4),
     ]
   };
 
   return (
     <div className="App">
-      <CourseView {...course} />
+      <RoundView {...round} />
     </div>
   );
 }
