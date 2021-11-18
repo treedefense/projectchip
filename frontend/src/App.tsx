82bb1ddcd926bc50609a040a390c5a984979bc33
Phil <pahartman1@crimson.ua.edu>
2021-11-17 18:56:42 -0800

    Added ability to choose courses, but only displays pars, doesn't track strokes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
 
+interface CourseList {
+  courseChoice: number;
+  setCourse(courseChoice: number): void;
+  courses: Record<number, Course>;
+}
+
 interface Course {
   id: number; // unique course id
   name: string,
@@ -13,113 +19,103 @@ interface Hole {
   par: number;
 }
 
-/*
-  strokes: number;
-  setStrokes(strokes: number): void;
-*/
-
-// rename to match?
-// match is N rounds
-
 interface Round {
   courseId: number;
-  strokes: Record<number, number>; // hole id to strokes
+  holes: Record<string, Hole>;
 }
 
 function HoleView(hole: Hole) {
-  /*
-  if (hole.strokes > 0) {
-    return <p>Shot {hole.strokes} on hole {hole.id}</p>
-  }
-
-  return (
-    <>
-      <p>Enter Strokes for hole {hole.id}</p>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((strokes) => {
-        return (
-          <button
-            key={strokes}
-            onClick={() => hole.setStrokes(strokes)}
-          >
-            {strokes}
-          </button>
-        )
-      })}
-    </>
-  )
-  */
 }
 
 function RoundView(round: Round) {
-  /*
-  let score = 0;
-  for (let i = 0; i < round.holes.length; i++) {
-    const hole = round.holes[i];
-    if (hole.strokes === 0) {
-      break;
-    }
-    score += hole.strokes - hole.par;
-  }
-  const scoreString = score <= 0 ? score.toString() : `+${score}`;
-
   return (
-    <>
     <div>
-      <p>Course name is {round.name}</p>
-    </div>
-    <div className="round">
-      {round.holes.map(hole => {
-        return <HoleView {...hole} key={hole.id} />
+      <p>{round.courseId}</p>
+      {Object.values(round.holes).map((hole) => {
+        return <p>{hole.par}</p>
       })}
     </div>
-    <div>
-      Score: {scoreString}
-    </div>
-    </>
   )
-  */
 }
 
-/*
-function useNewHole(id: number, par: number): Hole {
-    const [strokes, setStrokes] = useState(0)
-    return {
-      id,
-      par,
-      strokes,
-      setStrokes,
-    };
+function CoursePicker(courseList: CourseList) {
+  return (
+    <div>
+      {Object.values(courseList.courses).map((course)=> {
+        return (
+          <button
+          key={course.id}
+          onClick={() => courseList.setCourse(course.id)}
+          >
+          {course.name}
+          </button>
+        )
+      })}
+      <p>{courseList.courseChoice}</p>
+    </div>
+  )
 }
-*/
 
-function CoursePicker(courses: Record<number, Course>) {
-  return <div>Test</div>
-}
 
 function App() {
-  const courses: Record<number, Course> = {
-    1: {
-      id: 1,
-      name: 'cherry Hill',
-      location: 'CA',
-      holes: {
-        1: {
-          id: 1,
-          par: 3,
+  const [courseChoice, setCourse] = useState(-1)
+  const courseList: CourseList = {
+    courseChoice,
+    setCourse,
+    courses: {
+      1: {
+        id: 1,
+        name: 'Cherry Hill',
+        location: 'CA',
+        holes: {
+          1: {
+            id: 1,
+            par: 3,
+          },
+          2: {
+            id: 2,
+            par: 4,
+          },
         },
-        2: {
-          id: 2,
-          par: 4,
+      },
+      2: {
+        id: 2,
+        name: 'Orange Brook',
+        location: 'OR',
+        holes: {
+          1: {
+            id: 1,
+            par: 4,
+          },
+          2: {
+            id: 2,
+            par: 3,
+          },
         },
       },
     },
   };
 
-  return (
-    <div className="App">
-      <CoursePicker {...courses} />
-    </div>
-  );
+
+  if(courseList.courseChoice === -1)
+  {
+    return (
+      <div className="App">
+        <CoursePicker{...courseList}/>
+      </div>
+    );
+  } else {
+    let round: Round = {
+      courseId: courseChoice,
+      holes: courseList.courses[courseChoice].holes,
+      }
+    return (
+      <div className="App">
+        <RoundView{...round}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
