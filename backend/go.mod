a51b7e31076c88ad83e0cbc5c2f074973d246527
Phil <pahartman1@crimson.ua.edu>
2022-01-13 19:16:22 -0800

    set cors on backend so frontend could connect to it

--- backend/go.mod
+++ backend/go.mod
@@ -5,16 +5,17 @@ go 1.17
 require (
 	github.com/99designs/gqlgen v0.14.0
 	github.com/ethanpailes/pgtypes v0.0.0-20210319175856-9f6ab13c3655
+	github.com/gorilla/websocket v1.4.2
 	github.com/jackc/pgconn v1.10.1
 	github.com/jackc/pgx/v4 v4.14.1
 	github.com/opendoor/pggen v0.3.2
+	github.com/rs/cors v1.6.0
 	github.com/vektah/gqlparser/v2 v2.2.0
 )
 
 require (
 	github.com/agnivade/levenshtein v1.1.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
--- backend/server.go
+++ backend/server.go
@@ -10,8 +10,11 @@ import (
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/websocket"
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/rs/cors"
 	"github.com/treedefense/projectchip/db"
 	"github.com/treedefense/projectchip/graph"
 	"github.com/treedefense/projectchip/resolvers"
@@ -44,10 +47,29 @@ func main() {
 		},
 	}
 
+	// Add CORS middleware around every request
+	// See https://github.com/rs/cors for full option listing
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	})
+
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(schemaConfig))
 
+	srv.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				// Check against your desired domains here
+				return r.Host == "example.org"
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	})
+
 	http.Handle("/playground", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", c.Handler(srv))
 
 	log.Printf("connect to http://localhost:%s/playground for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
