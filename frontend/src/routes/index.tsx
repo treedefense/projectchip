fd4fa7635f154187260d464964d0ec6d7846b993
Ronnie Smith <halfpint1170@gmail.com>
2022-06-14 22:04:11 -0700

    progress point before switching a fair bit up in the backend

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { Routes as ReactRoutes, Route, Outlet, Link } from 'react-router-dom';
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 
 import { Home } from './Home';
 import { NoMatch } from './NoMatch';
@@ -9,10 +9,10 @@ import { ProtectedRoute } from '../utils/auth';
 import './index.css';
 
 import {
-    Play,
-    LocationPicker,
-    CoursePicker,
-    HolePicker,
+	Play,
+	LocationPicker,
+	CoursePicker,
+	HolePicker,
 } from './Play';
 
 import * as paths from './paths';
@@ -20,61 +20,62 @@ import * as paths from './paths';
 // Route the paths to the proper route element.
 // Heavily tied into the layout
 export function Routes() {
-    const { isLoading } = useAuth0();
+	// const { isLoading } = useAuth0();
 
-    if (isLoading) {
-        return <div> Loading auth </div>;
-    }
+	// if (isLoading) {
+	// return <div> Loading auth </div>;
+	// }
 
-    return (
-        <ReactRoutes>
-            <Route path={paths.homePath} element={<Layout />}>
-                <Route index element={<Home />} />
+	return (
+		<ReactRoutes>
+			<Route path={paths.homePath} element={<Layout />}>
+				<Route index element={<Home />} />
 
-                <Route path={paths.matchesPath} element={<ProtectedRoute component={Matches} />}>
-                    <Route index element={<MatchPicker />} />
-                    <Route path={paths.matchPath} element={<Match />} />
-                </Route>
+				<Route path={paths.matchesPath} element={<ProtectedRoute component={Matches} />}>
+					<Route index element={<MatchPicker />} />
+					<Route path={paths.matchPath} element={<Match />} />
+				</Route>
 
-                <Route path={paths.playPath} element={<ProtectedRoute component={Play} />} >
-                    <Route index element={<LocationPicker />} />
-                    <Route path={paths.coursePickerPath} element={<CoursePicker />} />
-                    <Route path={paths.holePickerPath} element={<HolePicker />} />
-                </Route>
+				<Route path={paths.playPath} element={<ProtectedRoute component={Play} />} >
+					<Route index element={<LocationPicker />} />
+					<Route path={paths.coursePickerPath} element={<CoursePicker />} />
+					<Route path={paths.holePickerPath} element={<HolePicker />} />
+				</Route>
 
-                <Route path="*" element={<NoMatch />} />
-            </Route>
-        </ReactRoutes>
-    )
+				<Route path="*" element={<NoMatch />} />
+			</Route>
+		</ReactRoutes>
+	)
 }
 
 function Layout() {
-    const { isAuthenticated } = useAuth0();
-    return (
-        <>
-            <nav>
-                <Link className="nav-link" to={paths.homePath}>Project Chip</Link>
-                {!isAuthenticated && (<>
-                    <div
-                        className="nav-link"
-                        onClick={() => window.location.pathname = paths.loginPath}
-                    >
-                        Login
-                    </div>
-                </>)}
-                {isAuthenticated && (<>
-                    <Link className="nav-link" to={paths.matchesPath}>Matches</Link>
-                    <Link className="nav-link" to={paths.playPath}>Play</Link>
-                    <div
-                        className="nav-link"
-                        onClick={() => window.location.pathname = paths.logoutPath}
-                    >
-                        Logout
-                    </div>
-                </>)}
-            </nav>
-            <Outlet />
-        </>
-    );
+	// const { isAuthenticated } = useAuth0();
+	const isAuthenticated = false;
+	return (
+		<>
+			<nav>
+				<Link className="nav-link" to={paths.homePath}>Project Chip</Link>
+				{!isAuthenticated && (<>
+					<div
+						className="nav-link"
+						onClick={() => window.location.pathname = paths.loginPath}
+					>
+						Login
+					</div>
+				</>)}
+				{isAuthenticated && (<>
+					<Link className="nav-link" to={paths.matchesPath}>Matches</Link>
+					<Link className="nav-link" to={paths.playPath}>Play</Link>
+					<div
+						className="nav-link"
+						onClick={() => window.location.pathname = paths.logoutPath}
+					>
+						Logout
+					</div>
+				</>)}
+			</nav>
+			<Outlet />
+		</>
+	);
 }
 
--- frontend/src/routes/paths.tsx
+++ frontend/src/routes/paths.tsx
@@ -1,3 +1,6 @@
+export const loginPath: string = '/' + process.env.REACT_APP_LOGIN_PATH;
+export const logoutPath: string = '/' + process.env.REACT_APP_LOGOUT_PATH;
+
 export const homePath = '/';
 export const matchesPath = '/matches';
 export const matchIdKey = 'matchId';
@@ -5,5 +8,3 @@ export const matchPath = `${matchesPath}/:${matchIdKey}`;
 export const playPath = '/play';
 export const coursePickerPath = playPath + '/courses';
 export const holePickerPath = playPath + '/holes';
-export const loginPath = '/login';
-export const logoutPath = '/logout';
